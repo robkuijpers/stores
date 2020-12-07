73ed0a69fc02fd9f96587df3cf6ac95a28293a56
Rob Kuijpers <dekuijpers@gmail.com>
2020-12-07 21:39:59 +0100

    feat: added okta oath

--- apps/bike-store/src/app/app.component.ts
+++ apps/bike-store/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { OktaAuthService } from '@okta/okta-angular';
+import { Component, OnInit } from '@angular/core'
+import { OktaAuthService } from '@okta/okta-angular'
 
 @Component({
   selector: 'stores-root',
@@ -7,48 +7,39 @@ import { OktaAuthService } from '@okta/okta-angular';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  title = 'Bike Store'
+  isAuthenticated: boolean
+  userName: string
 
-  title = 'Bike Store';
-  isAuthenticated: boolean;
-  userName: string;
-
-  constructor(public oktaAuth: OktaAuthService) { }
+  constructor(public oktaAuth: OktaAuthService) {}
 
   ngOnInit() {
-     this.oktaAuth.$authenticationState.subscribe(
-      (isAuthenticated) => {
-        this.isAuthenticated = isAuthenticated;
-         //const userClaims = await this.oktaAuth.getUser();
-         //this.userName = userClaims.name;
-      }
-    );
+    //  this.oktaAuth.$authenticationState.subscribe(
+    //   (isAuthenticated) => {
+    //     this.isAuthenticated = isAuthenticated;
+    //   }
+    // );
   }
 
   async login() {
-    // this.oktaAuth.signInWithRedirect({
-    //   originalUri: '/profile'
-    // });
-    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
-    if (this.isAuthenticated) {
-      const userClaims = await this.oktaAuth.getUser();
-      this.userName = userClaims.name;
-    }
+    // // this.oktaAuth.signInWithRedirect({
+    // //   originalUri: '/profile'
+    // // });
+    // this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+    // if (this.isAuthenticated) {
+    //   const userClaims = await this.oktaAuth.getUser();
+    //   this.userName = userClaims.name;
+    // }
   }
 
   async logout() {
-      const issuer = 'https://dev-1323293.okta.com/oauth2/default';
-      const redirectUri = `${window.location.origin}/welcome`;
-
-      // Read idToken before local session is cleared
-      const idToken = await this.oktaAuth.getIdToken();
-
-      // Clear local session
-      await this.oktaAuth.signOut('/');
-
-      // Clear remote session
-      window.location.href = `${issuer}/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=${redirectUri}`;
-
-    // this.oktaAuth.signOut();
+    // const issuer = 'https://dev-1323293.okta.com/oauth2/default';
+    // const redirectUri = `${window.location.origin}/welcome`;
+    // // Read idToken before local session is cleared
+    // const idToken = await this.oktaAuth.getIdToken();
+    // // Clear local session
+    // await this.oktaAuth.signOut('/');
+    // // Clear remote session
+    // window.location.href = `${issuer}/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=${redirectUri}`;
   }
-
 }
--- apps/bike-store/src/app/welcome/welcome.component.ts
+++ apps/bike-store/src/app/welcome/welcome.component.ts
@@ -1,45 +1,42 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { OktaAuthService } from '@okta/okta-angular';
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { OktaAuthService } from '@okta/okta-angular'
 @Component({
-    templateUrl: './welcome.component.html',
-    styleUrls: ['./welcome.component.scss']
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit, OnDestroy {
+  title = 'Welcome'
+  isAuthenticated = false
 
-    title = 'Welcome';
-    isAuthenticated = false;
+  constructor(private oktaAuth: OktaAuthService) {}
 
-    constructor(private oktaAuth: OktaAuthService) { }
+  ngOnInit(): void {
+    this.oktaAuth
+      .isAuthenticated()
+      .then((auth) => {
+        this.isAuthenticated = auth
+      })
+      .catch((err) => {
+        console.log('Error')
+      })
 
-    ngOnInit(): void {
-        this.oktaAuth.isAuthenticated().then((auth) => {
-            this.isAuthenticated = auth;
-        }).catch((err) => {
-            console.log('Error');
-        });
+    // subscribe to authentication state changes
+    this.oktaAuth.$authenticationState.subscribe((isAuthenticated: boolean) => (this.isAuthenticated = isAuthenticated))
+  }
 
-        // subscribe to authentication state changes
-        this.oktaAuth.$authenticationState.subscribe(
-          (isAuthenticated: boolean)  => this.isAuthenticated = isAuthenticated
-        );
-    }
+  async logout() {
+    const issuer = 'https://dev-1323293.okta.com/oauth2/default'
+    const redirectUri = `${window.location.origin}/welcome`
 
-    async logout() {
-        const issuer = 'https://dev-1323293.okta.com/oauth2/default';
-        const redirectUri = `${window.location.origin}/welcome`;
+    // Read idToken before local session is cleared
+    const idToken = await this.oktaAuth.getIdToken()
 
-        // Read idToken before local session is cleared
-        const idToken = await this.oktaAuth.getIdToken();
+    // Clear local session
+    await this.oktaAuth.signOut()
 
-        // Clear local session
-        await this.oktaAuth.signOut();
+    // Clear remote session
+    window.location.href = `${issuer}/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=${redirectUri}`
+  }
 
-        // Clear remote session
-        window.location.href = `${issuer}/v1/logout?id_token_hint=${idToken}&post_logout_redirect_uri=${redirectUri}`;
-
-      // this.oktaAuth.signOut();
-    }
-
-    ngOnDestroy(): void { }
-
+  ngOnDestroy(): void {}
 }
