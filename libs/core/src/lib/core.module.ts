1ad0249372c821d8b7949a8e9381c1270d85b25e
Rob Kuijpers <dekuijpers@gmail.com>
2020-12-08 20:41:05 +0100

    added nestjs and graphql

---
+++ libs/core/src/lib/core.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common'
+
+import { ConfigModule } from '@nestjs/config'
+import { configuration } from './config/configuration'
+import { validationSchema } from './config/validation'
+import { GraphQLModule } from '@nestjs/graphql'
+import { CoreResolver } from './core.resolver'
+
+// http://localhost:3000/graphql
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+  ],
+  controllers: [],
+  providers: [CoreResolver],
+  exports: [],
+})
+export class CoreModule {}
